7e0a2a4b7fbfdedb0373dd57e4c64bbb37094c3b
M-Sn <muhannad@optimaizai.com>
2025-03-07 12:39:19 +0100

    fix 2

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ let vue = new Vue({
             const minutes = String(date.getMinutes()).padStart(2, '0');
             const seconds = String(date.getSeconds()).padStart(2, '0');
 
-            return `${day}.${month}. ${hours}:${minutes}:${seconds}`;
+            return `${day}.${month}. ${hours}:${minutes}`;
         },
 
         getLogUrl() {
